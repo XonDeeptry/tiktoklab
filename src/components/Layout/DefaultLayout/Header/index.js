8af7ba8faf601b7c3d68b2694c00bcc1e7d0ec38
XonDeeptry <xondeeptry@gmail.com>
2022-07-19 15:23:29 +0700

    Update for Default Layout

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// bind styles hỗ trợ viết classname dưới dạng dấu -
+const cx = classNames.bind(styles);
+
 function Header() {
-    return ( <h2>Header</h2> );
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                {/* Search */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
