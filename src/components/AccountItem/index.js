12fcd21547d0c2eee162741df7ec2c08986ced1f
XonDeeptry <xondeeptry@gmail.com>
2022-07-21 07:36:22 +0700

    Xây dựng UI phần 2 hoàn thành xong xây dựng nút search

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,24 @@
+import classNames from 'classnames/bind';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './AccountItem.module.scss';
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img src="" alt="Hoaa" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Username</span>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>Bùi văn X</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,32 +1,65 @@
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
 import images from '~/assests/images';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 
 // bind styles hỗ trợ viết classname dưới dạng dấu -
 const cx = classNames.bind(styles);
 
 function Header() {
+    // Tạo state
+    const [searchResult, setSearchResult] = useState([]);
+    // Fake API for array
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 0);
+    });
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok" />
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder="Search accounts and Videos" spellCheck="false" />
-                    <button className={cx('clear-btn')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                <Tippy
+                    interactive={true}
+                    // Hiển thi thị searchResult len > 0
+                    visible={searchResult.length > 0}
+                    // Giao diện của dropdown
+                    render={(attrs) => (
+                        <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+                            {/* Import Wrapper của Popper và tùy chỉnh tại css của Header theo nhu cầu */}
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}>
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and Videos" spellCheck="false" />
+                        <button className={cx('clear-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-                    <button className={cx('searcth-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+                        <button className={cx('searcth-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={cx('actions')}></div>
             </div>
         </header>
