3546f0b66994f95603512742733a5a6b52a8f5d7
XonDeeptry <xondeeptry@gmail.com>
2022-07-24 16:36:05 +0700

    Xây dựng custom font, custom Icons, Custom Image component

--- src/assests/images/index.js
+++
@@ -1,6 +0,0 @@
-const images = {
-    // svg trả về một object có attribute default chứa đường dẫn tới file đo dó cần phải .default để lấy attribute đó ra
-    logo: require('~/assests/images/logo.svg').default,
-};
-
-export default images;
